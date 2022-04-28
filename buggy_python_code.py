3a66f74ba25a9e64eb584efb44cda4815530e41b
Jan Komárek <kom.honza@gmail.com>
2022-04-28 10:47:42 +0200

    fixed some more bugs

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,5 +1,6 @@
 # contains bunch of buggy examples
 # taken from https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
+
 import subprocess
 import base64
 
@@ -8,18 +9,20 @@ try:
 except ImportError:
     import pickle
 
-from flask import Flask
+from flask import Flask, request
 
 
 # Input injection
 def transcode_file(request, filename):
+    assert request
     command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
     subprocess.call(command, shell=True)  # a bad idea!
 
 
 # Assert statements
-def foo(request, user):
+def access_check(request, user):
     assert user.is_admin, 'user does not have access'
+    assert request
     # secure code...
 
 
@@ -32,12 +35,13 @@ class RunBinSh:
 def import_urlib_version(version):
     exec("import urllib%s as urllib" % version)
 
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    module = flask.request.args.get("module")
+    module = request.args.get("module")
     import_urlib_version(module)
 
 
